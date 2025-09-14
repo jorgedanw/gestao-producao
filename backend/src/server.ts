936db9bf43efdf911f19897f083272cbbc565e4f
Seu Nome <seuemail@empresa.com>
2025-09-14 02:35:15 -0300

    M1: estrutura inicial + Hello API/Web

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,11 @@ import Fastify from "fastify";
 
 const app = Fastify();
 
+// Rota simples só para testar
 app.get("/hello", async () => {
   return { message: "Hello API 🚀" };
 });
 
-app.listen({ port: 3000 }, () => {
+app.listen({ port: 3000 }).then(() => {
   console.log("Server running on http://localhost:3000");
 });
